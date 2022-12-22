a4395eb0a18f001974ad37b10c092c1bc62c0ad8
Tom Knobel <tom.knobel@orca.security>
2022-12-22 14:32:24 +0200

    add litel server with vulns 3

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def show_secret():
+	GITHUB_PERSONAL_TOKEN = "ghp_ZRrWptP6rQQq0pSuiSh15HzmAdrIVV2zkXNp"
+	print("GITHUB_PERSONAL_TOKEN")
+	print(GITHUB_PERSONAL_TOKEN)
+
+
+if __name__ == "__main__":
+	show_secret()
+
